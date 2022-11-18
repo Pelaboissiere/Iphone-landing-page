56df1e867cbe3865e0afde8da192bfc452ff3dd5
Pedro Laboissiere <pelaboissiere@gmail.com>
2022-11-18 10:04:48 -0300

    First commit

---
+++ Iphone/script.js
@@ -0,0 +1,12 @@
+/*Função de mudar a cor do celular*/
+
+    function imgSlider(e){
+        document.querySelector('#phone').src = e;
+    }
+
+/*Função de mudar a cor do circulo*/
+
+    function circleChange(color){
+        const circle = document.querySelector('.circle');
+        circle.style.background = color;
+    }
